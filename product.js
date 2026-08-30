0a8a8cc75cf0204369226a5abb3e80d16b98c7d3
youssefelkadaoui <youssefelkadaoui981@gmail.com>
2026-08-30 17:58:45 +0100

    Update product.js

--- product.js
+++ product.js
@@ -7,7 +7,7 @@ function fmtPrice(n) {
   try { return n.toLocaleString('ar'); } catch (e) { return String(n); }
 }
 
-const googleSheetsEndpoint = 'https://script.google.com/macros/s/AKfycby4-w47zYJuASb-EFNMrf8ORx3lQtTj5lfdyCbh-3aYe7933Kgw7Zod9wKVEBMxEgRH8g/exec';
+const googleSheetsEndpoint = 'https://script.google.com/macros/s/AKfycbxNN0LYs-KQsS2zG2iqA_pjPqOSRYKQU0VDUwecyZcYGvNGUrGbeKObYu8TP2gDSE-4zg/exec';
 
 function sendOrderToGoogleSheets(order) {
   const form = document.createElement('form');
@@ -230,7 +230,7 @@ checkoutForm.addEventListener('submit', async (e) => {
   document.getElementById('formTotal').value = totalText;
 
   try {
-    await fetch('https://script.google.com/macros/s/AKfycbyJHi3EXEUG6JaMoSp-_RmgAMzNTLEzwGXrE675h9JLaXtpxVS_7Xbg0I4C1cOOGH0X0A/exec', {
+    await fetch(googleSheetsEndpoint, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
